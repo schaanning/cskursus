4d839ad576aa62a1fcbe1b0b64f9bbaab2d4b51b
Rolf <admin@AR-114-05>
2018-05-23 12:07:12 +0200

    opgave5

---
+++ Arrays/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] løn = { 20000, 15000, 20000, 5000, 40000, 7000, 60000, 59000 };
+
+            for (int i = 0; i < løn.Length; i++)
+            {
+                Console.WriteLine(løn[i]);
+            }
+
+            Console.WriteLine("Gennemsnits løn: " + løn.Average());
+
+            Array.Sort(løn);
+            løn[1] = 10;
+            for (int i = 0; i < løn.Length; i++)
+            {
+                Console.WriteLine(løn[i]);
+            }
+
+
+            System.Console.ReadLine();
+
+
+            //*****************************************************************
+                // 
+                //*****************************************************************
+
+
+        }
+    }
+}
---
+++ opgave5/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace opgave5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string fornavn = "Rolf";
+            string efternavn = "Schaanning";
+
+            string samletNavn = fornavn + " " + efternavn;
+            Console.WriteLine(samletNavn);
+
+            string navnStort = samletNavn.ToUpper();
+            Console.WriteLine(navnStort);
+
+            string navnLille = samletNavn.ToLower();
+            Console.WriteLine(navnLille);
+
+            string del = samletNavn.Substring(6, 3);
+            Console.WriteLine(del);
+
+            string[] arrayNavn = samletNavn.Split(' ');
+
+            for (int i = 0; i < arrayNavn.Length; i++)
+            {
+                Console.WriteLine(arrayNavn[i]);
+            }
+
+            string text1 = "tewr\tklods\r\netyu";
+            Console.WriteLine(text1);
+
+            System.IO.File.WriteAllText("c:\\temp\\opgave5.txt", text1);
+
+            System.Console.ReadLine();
+
+
+        }
+    }
+}
