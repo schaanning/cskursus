65eb8abbb9b1c4d2bf94ec65e9cf04d07883384b
Rolf <admin@AR-114-05>
2018-05-23 13:42:51 +0200

    opgave6 lavet

---
+++ opgave6/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace opgave6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string talString;
+            for (int i1 = 1; i1 < 11; i1++)
+            {
+                for (int i2 = 1; i2 < 11; i2++)
+                {
+                    //string talString = (i1*i2).ToString();
+                    talString = (i1 * i2).ToString();
+
+                    if (i1 * i2 > 50)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                    }
+
+                    Console.Write(talString.PadLeft(4));
+                }
+                Console.WriteLine();
+            }
+
+            System.Console.ReadLine();
+
+        }
+    }
+}
