99ebb00441b66ec524a18aedee356e379387a60d
Rolf Schaanning <schaanning@hotmail.com>
2018-06-06 08:08:13 +0200

    Hjemmeopgave1 er lavet

---
+++ Hjemmeopgave1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hjemmeopgave1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person k1 = new Person();
+            k1.navn = "Rolf";
+            Person k2 = new Person();
+            k2.navn = "Karina";
+
+
+            Console.WriteLine(k1.navn);
+            Console.WriteLine(k2.navn);
+
+            k1 = k2;
+
+            Console.WriteLine(k1.navn);
+            Console.WriteLine(k2.navn);
+
+            k1.navn = "Schaanning";
+
+            Console.WriteLine(k1.navn);
+            Console.WriteLine(k2.navn);
+
+            System.Console.ReadLine();
+        }
+
+        class Person
+        {
+            public string navn;
+        }
+    }
+}
