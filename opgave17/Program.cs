fb462ef76a3e34132bde63d1f66c9e3f0bfe578a
rsch <rsch@danskebank.dk>
2018-06-07 11:10:44 +0200

    Opgave 17 og 18 er lavet

---
+++ opgave17/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace opgave17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person() { Fornavn = "Rolf", Efternavn = "Schaanning"};
+
+            Elev e1 = new Elev() { Fornavn = "Asker", Efternavn = "Rasmussen", Klasselokale = "Lakale1" };
+
+            Instruktør i1 = new Instruktør() { Fornavn = "Karina", Efternavn = "Wiedenhofer", NøgleId = 4 };
+
+            Console.WriteLine(p1.FuldNavn());
+            Console.WriteLine(e1.FuldNavn());
+            Console.WriteLine(i1.FuldNavn());
+
+            Console.ReadLine();
+        }
+    }
+
+    class Person
+    {
+        public string Fornavn { get; set; }
+        public string Efternavn { get; set; }
+
+        public string FuldNavn ()
+        {
+            return Fornavn + " " + Efternavn + " er en person";
+        }
+    }
+
+    class Elev : Person
+    {
+        public string Klasselokale { get; set; }
+
+        public string FuldNavn()
+        {
+            return Fornavn + " " + Efternavn + " er elev";
+        }
+    }
+
+    class Instruktør : Person
+    {           
+        public int NøgleId { get; set; }
+        public string FuldNavn()
+        {
+            return Fornavn + " " + Efternavn + " er instruktør";
+        }
+    }
+}
---
+++ opgave18/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace opgave18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UdvidetRandom u1 = new UdvidetRandom();
+
+            for (int ix = 1; ix < 100; ix++)
+            {
+                Console.WriteLine(u1.NextBool());
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+
+    class UdvidetRandom : System.Random
+    {
+        public bool NextBool ()
+        {
+            return this.Next(1, 1001) < 500;
+        }
+    }
+}
