12154e4dd773486c82e2b20ca81c6156c3038f1d
rsch <rsch@danskebank.dk>
2018-06-07 15:26:48 +0200

    Opgave 21 og 22 er lavet

---
+++ opgave21/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace opgave21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Run();
+            }
+            catch (Exception MinFejlMeddelelse)
+            {
+                Console.WriteLine("Der er sket en fejl - " + MinFejlMeddelelse.Message);
+            }
+            
+
+            Console.ReadLine();
+        }
+
+        private static void Run()
+        {
+            Console.WriteLine("Indtast 1. tal");
+            int tal1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Indtast 2. tal");
+            int tal2 = Convert.ToInt32(Console.ReadLine());
+            int res = tal1 + tal2;
+            Console.WriteLine("resultatet er " + res);
+        }
+
+    }
+}
---
+++ opgave22/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace opgave22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                StockItem s = new StockItem(-1);
+            }
+            catch (StockItemExeption ex)
+            {
+                Console.WriteLine("StockItem fejl - " + ex.Message);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Generel fejl");
+                throw;
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    public class StockItem
+    {
+        public int ID { get; set; }
+
+        public StockItem(int id)
+        {
+            if (id < 0)
+                throw new StockItemExeption("wrong ID");
+        }
+    }
+
+    public class StockItemExeption : Exception
+    {
+        public StockItemExeption(string message) : base(message)
+        {
+        }
+
+    }
+}
