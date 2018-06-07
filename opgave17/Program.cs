e1bbef20c51af73708065f83185b1ddca0c0e617
rsch <rsch@danskebank.dk>
2018-06-07 14:00:42 +0200

    opgave 20 lavet

--- opgave17/Program.cs
+++ opgave17/Program.cs
@@ -14,9 +14,18 @@ namespace opgave17
             Elev       e1 = new Elev()       { Fornavn = "Asker",  Efternavn = "Rasmussen",   Klasselokale = "Lakale1" };
             Instruktør i1 = new Instruktør() { Fornavn = "Karina", Efternavn = "Wiedenhofer", NøgleId = 4 };
 
-            Console.WriteLine(p1.FuldNavn());
-            Console.WriteLine(e1.FuldNavn());
-            Console.WriteLine(i1.FuldNavnInstruktør());
+            Person[] lst = new Person[3];
+            lst[0] = p1;
+            lst[1] = e1;
+            lst[2] = i1;
+
+            // Console.WriteLine(p1.FuldNavn());
+            // Console.WriteLine(e1.FuldNavn());
+            // Console.WriteLine(i1.FuldNavnInstruktør());
+
+            Console.WriteLine(lst[0].FuldNavn());
+            Console.WriteLine(lst[1].FuldNavn());
+            Console.WriteLine(lst[2].FuldNavn());
 
             Console.ReadLine();
         }
@@ -46,7 +55,11 @@ namespace opgave17
     class Instruktør : Person
     {           
         public int NøgleId { get; set; }
-        public string FuldNavnInstruktør()
+        //public string FuldNavnInstruktør()
+        //{
+        //    return Fornavn + " " + Efternavn + " er instruktør";
+        //}
+        public override string FuldNavn()
         {
             return Fornavn + " " + Efternavn + " er instruktør";
         }
---
+++ opgave20/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace opgave20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Hund h1 = new Hund() { Navn = "Rolf"};
+            //Kat k1 = new Kat() { Navn = "Karina"};
+
+            //h1.SigNoget();
+            //k1.SigNoget();
+
+            Dyr[] dyr = new Dyr[20];
+            for (int ix = 0; ix < 20; ix++)
+                dyr[ix] = Dyr.TilfældigtDyr();
+            foreach (Dyr MitDyr in dyr)
+            {
+                MitDyr.SigNoget();
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+
+    abstract class Dyr
+    {
+        static System.Random rnd = new Random();
+
+        public string Navn { get; set; }
+
+        public virtual void SigNoget()
+        {
+            Console.WriteLine("Jeg er et dyr og hedder " + this.Navn);
+        }
+
+        public static Dyr TilfældigtDyr()
+        {
+            string sti = @"X:\dyrenavne.txt";
+            string[] navne = System.IO.File.ReadAllLines(sti);
+            int index = rnd.Next(0, navne.Length);
+            if (rnd.Next(1, 3) == 1)
+            {
+                return new Hund { Navn = navne[index] };
+            }
+            else
+            {
+                return new Kat { Navn = navne[index] };
+
+            }
+
+        }
+    }
+
+    class Hund : Dyr
+    {
+        public override void SigNoget()
+        {
+            Console.WriteLine("Jeg er en hund og hedder " + this.Navn);
+        }
+
+    }
+
+    class Kat : Dyr
+    {
+        public override void SigNoget()
+        {
+            Console.WriteLine("Jeg er en kat og hedder " + this.Navn);
+        }
+
+    }
+}
