f2bde1ce76315c9d53b9765c438881915bde8067
Rolf Schaanning <schaanning@hotmail.com>
2018-06-19 14:39:38 +0200

    Opgave28 er lavet

---
+++ opgave27/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace opgave27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Collections.Generic.List<Person> lp1 = new List<Person>();
+            lp1.Add(new Person() { Id = 1, Navn = "Rolf" });
+            lp1.Add(new Person() { Id = 2, Navn = "Karina" });
+            lp1.Add(new Person() { Id = 3, Navn = "PV" });
+
+            foreach (Person item in lp1)
+            {
+                Console.WriteLine(item.Navn);
+            }
+
+            System.Collections.Generic.Dictionary<int, Person> dp1 = new Dictionary<int, Person>();
+            dp1.Add(1, new Person() { Id = 11, Navn = "Flemming" });
+            dp1.Add(2, new Person() { Id = 21, Navn = "Stine" });
+            dp1.Add(3, new Person() { Id = 31, Navn = "Asker" });
+
+            var p = dp1[1];
+            Console.WriteLine(p.Navn);
+
+
+            Console.ReadLine();
+        }
+    }
+
+    class Person
+    {
+        public int Id { get; set; }
+        public string Navn { get; set; }
+    }
+}
---
+++ opgave28/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace opgave28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bunke b = new Bunke();
+            Console.WriteLine(b);
+            b.TilføjKort(new Kort() { Kulør = "Spar", Værdi = 4 });
+            b.TilføjKort(new Kort() { Kulør = "Hjerter", Værdi = 8 });
+            b.TilføjKort(new Kort() { Kulør = "Klør", Værdi = 6 });
+            b.TilføjKort(new Kort() { Kulør = "Ruder", Værdi = 2 });
+            b.Vis();
+
+            var k = b.FjernKort();
+            Console.WriteLine();
+            Console.WriteLine(k);
+            Console.WriteLine();
+            b.Vis();
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Kort
+    {
+        public string Kulør { get; set; }
+        public int Værdi { get; set; }
+        public override string ToString()
+        {
+            return this.Kulør + " " + this.Værdi;
+        }
+    }
+
+    public class Bunke
+    {
+        private Stack<Kort> bunke = new Stack<Kort>();
+
+        public void TilføjKort(Kort k)
+        {
+            bunke.Push(k);
+        }
+
+        public Kort FjernKort()
+        {
+            return bunke.Pop();
+        }
+
+        public void Vis()
+        {
+            foreach (var item in bunke)
+            {
+                //Console.WriteLine(item.Kulør + " " + item.Værdi);
+                Console.WriteLine(item.ToString());
+            }
+        }
+    }
+}
