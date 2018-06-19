43878555bdbe8bf17591a2d12f3247b05ce4fe19
Rolf Schaanning <schaanning@hotmail.com>
2018-06-19 11:47:52 +0200

    Opgave26 er lavet

---
+++ opgave26/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace opgave26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (System.IO.StreamReader stream = System.IO.File.OpenText(@"x:\dyrenavne.txt"))
+            //System.IO.StreamReader stream = System.IO.File.OpenText(@"x:\dyrenavne.txt");
+                while (stream.Peek() != -1)
+            {
+                string navn = stream.ReadLine();
+                Console.WriteLine(navn);
+            }
+
+            //stream.Close();
+            //stream = null;
+
+            Console.ReadLine();
+
+        }
+    }
+}
