66a77ca1c0a0566fcb56dbd6f6671b1f9ff12a77
Rolf Schaanning <schaanning@hotmail.com>
2018-06-20 12:50:05 +0200

    modificering af opgave34

---
+++ opgave33/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace opgave31
+{
+    //public delegate int Beregn(int a, int b);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int res;
+
+            res = Beregner(2, 4, Plus);
+            Console.WriteLine("Resultat: " + res);
+
+            res = Beregner(2, 4, Minus);
+            Console.WriteLine("Resultat: " + res);
+
+            res = Beregner(2, 4, Gange);
+            Console.WriteLine("Resultat: " + res);
+
+            res = Beregner(8, 4, Divider);
+            Console.WriteLine("Resultat: " + res);
+
+            Console.ReadLine();
+        }
+        public static int Beregner(int a, int b, Func<int, int, int> funktion)
+        {
+            return funktion(a, b);
+        }
+
+        public static int Plus(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Minus(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static int Divider(int a, int b)
+        {
+            return a / b;
+        }
+
+        public static int Gange(int a, int b)
+        {
+            return a * b;
+        }
+
+    }
+}
---
+++ opgave34/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace opgave31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double res;
+ 
+            res = Beregner(2, 4, FindTilfældigFunktion());
+            Console.WriteLine("Resultat: " + res);
+
+            //Lav en vent her.
+
+            res = Beregner(2, 4, FindTilfældigFunktion());
+            Console.WriteLine("Resultat: " + res);
+
+            //Lav en vent her.
+
+            res = Beregner(2, 4, FindTilfældigFunktion());
+            Console.WriteLine("Resultat: " + res);
+
+            //Lav en vent her.
+
+            res = Beregner(8, 4, FindTilfældigFunktion());
+            Console.WriteLine("Resultat: " + res);
+
+            Console.ReadLine();
+        }
+        public static double Beregner(double a, double b, Func<double, double, double> funktion)
+        {
+            return funktion(a, b);
+        }
+
+        public static double Plus(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Minus(double a, double b)
+        {
+            return a - b;
+        }
+
+        public static double Divider(double a, double b)
+        {
+            return a / b;
+        }
+
+        public static double Gange(double a, double b)
+        {
+            return a * b;
+        }
+
+        public static Func<double, double, double> FindTilfældigFunktion()
+        {
+            if (DateTime.Now.Millisecond < 250)
+                return Plus;
+            if (DateTime.Now.Millisecond < 500)
+                return Minus;
+            if (DateTime.Now.Millisecond < 750)
+                return Divider;
+            return Gange;
+        }
+
+    }
+}
