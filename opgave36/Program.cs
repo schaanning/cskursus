602e378eef1ca67fd28c3620706f872d6763413f
Rolf Schaanning <schaanning@hotmail.com>
2018-06-20 14:25:58 +0200

    Opgave36 er lavet

---
+++ opgave36/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace opgave36
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.IO.FileSystemWatcher w = new System.IO.FileSystemWatcher(@"c:\temp", "*.txt");
+            w.EnableRaisingEvents = true;
+            //w.Changed += FilÆndret;
+            w.Changed += (a, b) => { Console.WriteLine(b.Name); };
+            do
+            {
+
+            } while (true);
+
+        }
+
+        private static void FilÆndret(object sender, System.IO.FileSystemEventArgs e)
+        {
+            Console.WriteLine(e.Name);
+        }
+
+    }
+}
